cceef72c460bfccbc77a55831ded58bc93cffdf3
gabilima2004 <gabriellamvlima.20231@poli.ufrj.br>
2023-07-02 21:11:21 -0300

    Atualizando as modificações

--- capacitacao-1.js
+++ capacitacao-1.js
@@ -1,4 +1,4 @@
-class cliente {
+class Cliente {
     constructor(id, nome, pets, fidelizado){
         this.id = id;
         this.nome = nome;
@@ -7,7 +7,7 @@ class cliente {
     }
 }
 
-class funcionario {
+class Funcionario {
     constructor(id, usuario, senha){
         this.id = id;
         this.usuario = usuario;
@@ -15,7 +15,7 @@ class funcionario {
     }
 }
 
-class animal {
+class Animal {
     constructor(id, nomepet, nomedono, consultas){
         this.id = id;
         this.nomepet = nomepet;
@@ -24,7 +24,7 @@ class animal {
     }
 }
 
-class consulta {
+class Consulta {
     constructor(id, nomecliente, nomepet, funcionario, status, data){
         this.id = id;
         this.nomecliente = nomecliente;
@@ -36,56 +36,250 @@ class consulta {
 }
 
 const prompt = require('prompt-sync')({sigint:true})
-const listaClientes = []
+
 
 function cadastrarcliente() {
-    const id = prompt('Digite o ID do cliente: ');
+    //fazer o id
     const nome = prompt('Digite o nome do cliente: ');
     const pets = prompt('Digite os pets do cliente (separados por vírgula): ').split(',');
     const fidelizado = prompt('O cliente é fidelizado? (s/n)').toLowerCase() === 's';  
     const cliente = new Cliente(id, nome, pets, fidelizado);
-    listaClientes.push(novoCliente);
-    console.log('Novo cliente inserido:', novoCliente);
-    return cliente;
+    listaClientes.push(cliente);
+    console.log('Novo cliente inserido:', cliente);
+    return listaClientes;
 }
 
-function cadastrarfuncionario (){
+function cadastrarFuncionario (listaFuncionarios){
+    //verificar se o id ja existe
     const id = prompt('Digite o seu ID: ');
+    //verificar se o usuario ja existe
     const usuario = prompt('Digite o seu nome de usuário: ');
-    //verificar se o ja tem o usuario
     const senha = prompt('Digite a sua senha: ');
+    const funcionario = new Funcionario(id, usuario, senha);
+    listaFuncionarios.push(funcionario)
+    console.log('Novo funcionário inserido:', funcionario);
+    return listaFuncionarios;
 }
 
-function inserirlogin (){
-    const usuario = prompt('Digite o seu nome de usuário: ');
+function inserirlogin (listaFuncionarios){
+    //const id = prompt('Digite o seu ID: ');
+    let condicao = false
     //verificar se o usuario existe
-    const senha = prompt('Digite a sua senha: ');
+    for (let i = 0; i < listaFuncionarios.length; i++) {
+        if (listaFuncionarios.length == 0) {
+            console.log('Não há funcionários cadastrados.');
+            break;
+        }
+        if (i[0] == id) {
+            const usuario = prompt('Digite o seu nome de usuário: ');
+            condicao = true;
+        }
+    }
     //verificar se a senha bate com a cadastrada
-}
-function menu (){
-    //login funcionario
-    while (true){
-        try{
-            const resplogin = prompt('Você possui login?s/n ')
-            if (resplogin === 's') {
-                //funcao de login funcionario
-                inserirlogin();
-                break;
-            } else if (resplogin === 'n') {
-                //funcao de cadastrar funcionario
-                cadastrarfuncionario();
-                break;
-            } else {
-                throw new Error('Digite s ou n.');
+    let condicao1 = false;
+    if (condicao = true){
+        const senha = prompt('Digite a sua senha: ');
+        for (let i = 0; i < listaFuncionarios.length; i++) {
+            if (i[2] == senha) {
+                const funcionarioon = listaFuncionarios[i];
+                condicao1 = true;
+                return funcionarioon;
             }
-        } catch (error) {
-            console.error(error.message);
-        }  
-    }       
-    //aqui o funcionario ja vai ta logado e eu preciso associar o cadastro dele com as ações
+        }
+        if (condicao1== false) {
+            console.log('Senha inválida. Realize o login novamente.');
+        }
+    } else{
+        console.log('O usuário não foi encontrado. Realize cadastro:');
+    }      
 }
 
+function cadastraranimal() {
+    // fazer id
+    //const id = prompt('Digite o ID do pet: ');
+    const nomepet = prompt('Digite o nome do pet: ');
+    const nomecliente = prompt('Digite o nome do cliente: ');
+    const consultas = prompt('Digite as consultas realizadas: ');
+    const pet = new Animal(id, nomepet, nomecliente, consultas);
+    listaPets.push(pet);
+    console.log('Novo pet inserido:', pet);
+    return listaPets;
+}
 
-console.log(menu())
+function inserirconsulta() {
+    // fazer id
+    //const id = prompt('Digite o ID da consulta: ');
+    const nomecliente = prompt('Digite o nome do cliente: ');
+    const nomepet = prompt('Digite o nome do pet: ');
+    const status = prompt('Digite o status da consulta: ');
+    const funcionario = prompt('Digite o nome do funcionário que está realizando o agendamento: ');
+    const data = prompt('Digite a data da consulta: ')
+    const consulta = new Consulta(id, nomecliente, nomepet, status, funcionario, data);
+    listaConsultas.push(consulta);
+    console.log('Nova consulta agendada:', consulta);
+    return listaConsultas;
+}
+
+function menunaologado(){
+    console.log('Seja bem-vindx ao sistema de gerenciamento da veterinária!');
+    console.log('1 - Realizar login');
+    console.log('2 - Realizar cadastro');
+    console.log('3 - Sair');
+    while (true) {
+        try {
+          let opcao = prompt('Digite uma opção: ');
+    
+          if (opcao === '1' || opcao === '2' || opcao === '3') {
+            return opcao;
+          } else {
+            throw new Error('Resposta inválida.');
+          }
+        } catch (error) {
+          console.error('Ocorreu um erro:', error.message);
+        }
+    }
+}
+
+function menulogado(funcionarioon){
+    //let id = funcionarioon[0];
+    let nome = funcionarioon[1];
+    // senha = funcionarioon[2];
+    console.log('Olá ${nome}!');
+    console.log('1 - Ver meus dados');
+    console.log('2 - Modificar meus dados');
+    console.log('3 - Ver lista de clientes');
+    console.log('4 - Ver lista de pets');
+    console.log('5 - Ver lista de consultas');
+    console.log('6 - Ver lista de funcionários');
+    console.log('7 - Marcar consulta');
+    console.log('8 - Mudar status de consulta');
+    console.log('9 - Remover cliente');
+    console.log('10 - Remover pet');
+    console.log('11 - Cancelar consulta');
+    console.log('12 - Remover funcionário');
+    console.log('13 - Fazer logout');
+    while (true) {
+        try {
+          let opcao = prompt('Digite uma opção: ');
+    
+          if (opcao === '1' || opcao === '2' || opcao === '3' || opcao === '4' || opcao === '5' || opcao === '6' || opcao === '7' || opcao === '8' || opcao === '9' || opcao === '10' || opcao === '11' || opcao === '12'|| opcao === '13') {
+            return opcao;
+          } else {
+            throw new Error('Resposta inválida.');
+          }
+        } catch (error) {
+          console.error('Ocorreu um erro:', error.message);
+        }
+    }
+}
+
+function verdadosfuncionario(funcionarioon){
+    console.log('Opção: 1 - Ver meus dados');
+    //const id = funcionarioon[0]
+    const usuario = funcionarioon[1];
+    const senha = funcionarioon[2];
+    console.log('Seu usuário é ${usuario} e sua senha é ${senha}.');
+}
+
+function mudardadosfuncionario(funcionarioon){
+    console.log('Opção: 2 - Modificar meus dados');
+    console.log('1 - Usuário');
+    console.log('2 - Senha');
+    //let id = funcionarioon[0];
+    let usuario = funcionarioon[1];
+    let senha = funcionarioon[2];
+    while (true) {
+        try {
+          let opcao = prompt('Digite uma opção: ');
+    
+          if (opcao === '1' || opcao === '2') {
+            break;
+          } else {
+            throw new Error('Resposta inválida.');
+          }
+        } catch (error) {
+          console.error('Ocorreu um erro:', error.message);
+        }
+    }
+    if (opcao == '1'){
+        console.log('O seu usuário é ${usuario}.');
+        usuario = prompt('Insira o novo usuário.');
+    } else if (opcao == '2'){
+        console.log('A sua senha é ${senha}.');
+        usuario = prompt('Insira a nova senha.');
+    }
+    funcionarioon = [id, usuario, senha];
+    return funcionarioon
+}
+
+function verlistaclientes(listaClientes){
+    console.log('Opção: 3 - Ver lista de clientes');
+    //COLOCAR A LISTA EM ORDEM ALFABETICA
+    listaClientes.sort(function(a, b) {
+        const nomeA = a[1].toUpperCase();
+        const nomeB = b[1].toUpperCase();
+        if (nomeA < nomeB) {
+            return -1;
+          }
+          if (nomeA > nomeB) {
+            return 1;
+          }
+          return 0;
+        });
+
+    console.log(listaClientes);
+    return listaClientes
+}
+function main (){
+    let listaClientes = [];
+    let listaFuncionarios = [];
+    let listaPets = [];
+    let listaConsultas = [];
+    let funcionarioon = null;
+    // funcionario nao logado
+    while (true){
+        let opcaonlogin = menunaologado();
+        if (opcaonlogin == '1'){
+            funcionarioon = inserirlogin(listaFuncionarios);
+            //funcionario logado:
+            while (true){
+                let opcaonlogado = menulogado(funcionarioon);
+                if (opcaonlogado == '1'){
+                    verdadosfuncionario(funcionarioon)
+                }else if (opcaonlogado == '2'){
+                    funcionarioon = mudardadosfuncionario(funcionarioon)
+                }else if (opcaonlogado == '3'){
+                    listaClientes = verlistaclientes(listaClientes)
+                }else if (opcaonlogado == '4'){
+                    
+                }else if (opcaonlogado == '5'){
+                    
+                }else if (opcaonlogado == '6'){
+                
+                }else if (opcaonlogado == '7'){
+                    
+                }else if (opcaonlogado == '8'){
+                    
+                }else if (opcaonlogado == '9'){
+                    
+                }else if (opcaonlogado == '10'){
+                    
+                }else if (opcaonlogado == '11'){
+                    
+                }else if (opcaonlogado == '12'){
+                    
+                }else if (opcaonlogado == '13'){
+                    
+                }
+            }
+        }else if (opcaonlogin == '2'){
+            listaFuncionarios = cadastrarFuncionario (listaFuncionarios);
+        }else {
+            break
+        }    
+    } 
+}
+
+console.log(main())
 
 
